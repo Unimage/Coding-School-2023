9390fb3b1d4b550e7b32c929bc338f22ef670d9c
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-12 13:28:03 +0200

    Exercise 1 : Hello + Name

---
+++ Session-04/Session-04/ExerciseOne.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04 {
+    public class ExerciseOne {
+    
+        public string HelloMessage(){
+
+            string hello= "Hello ";
+            string name = "Stratos!";
+            string finalMessage = hello + name + "\n";
+        
+        return finalMessage;
+
+        }
+    }
+}
---
+++ Session-04/Session-04/Program.cs
@@ -0,0 +1,10 @@
+﻿using Session_04;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ExerciseOne exerOne = new ExerciseOne();
+        Console.WriteLine(exerOne.HelloMessage());
+        Console.ReadLine();
+    }
+}
