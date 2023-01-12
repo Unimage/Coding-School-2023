6f2d5daa8de3b54d25374a62422c42763ac47bee
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-12 13:39:33 +0200

    Exercise Two Add and Divide two numbers.

---
+++ Session-04/Session-04/ExerciseTwo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+    public class ExerciseTwo
+    {
+
+        public void SumAndDivition()
+        {
+            Console.WriteLine("Given numbers: 3 , 6");
+            Console.WriteLine("Sum: " + (3 + 6) + "\nDivision: " + (3 / (float)6) + "\n");
+        }
+    }
+}
--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -3,8 +3,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //1
         ExerciseOne exerOne = new ExerciseOne();
         Console.WriteLine(exerOne.HelloMessage());
+        
+
+        //2
+        ExerciseTwo exerTwo = new ExerciseTwo();
+        exerTwo.SumAndDivition();
         Console.ReadLine();
     }
 }
