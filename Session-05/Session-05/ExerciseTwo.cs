074dbe2996960827178726b5973f2ce3cb36374b
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-14 17:00:58 +0200

    Added Exercise Two : Sum and Product Calculation of a given number

---
+++ Session-05/Session-05/ExerciseTwo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class ExerciseTwo
+    {
+        public int Calculate(int inputNumber, int function)
+        {
+            int result = 0;
+            if (function == 1)
+            {
+                for (int i = 1; i <= inputNumber; i++)
+                {
+                    result += i;
+                }
+            }
+            else
+            {
+                result = 1;
+                for(int i=1; i<= inputNumber; i++)
+                {
+                    result *= i;
+                }
+
+            }
+        return result;
+        }
+
+    }
+}
--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -11,9 +11,37 @@ internal class Program
         ExerciseOne reverser = new ExerciseOne();
         ;
         Console.WriteLine("Reversed String : {0} ", reverser.ReverseString(input));
+
+        //2
+        Console.WriteLine("Enter a posivite integer:");
+        input = Console.ReadLine();
+        int inputInt;
+
+        try
+        {
+            inputInt = Convert.ToInt32(input);     
+        }
+        catch (System.FormatException)
+        {
+            inputInt = 0;
+            Console.WriteLine("Definitelly not an integer please try again.");
+        }
+        ExerciseTwo calculator = new ExerciseTwo();
+        Console.WriteLine("Please enter an function : \n1.Summary \n2.Product ");
+        string inputFunction = Console.ReadLine();
+        int function = Convert.ToInt32(inputFunction);
+        if(function == 1)
+        {
+            Console.WriteLine("The Sum of numbers from 1 to {0} is {1}", inputInt, calculator.Calculate(inputInt, function));
+        }
+        else if (function ==2)
+        {
+            Console.WriteLine("The Product of numbers from 1 to {0} is {1}", inputInt, calculator.Calculate(inputInt, function));
+        }
+        else
+        {
+            Console.WriteLine("Error at choosing function. either press 1 or 2.");
+        }
         Console.ReadLine();
-
-
-        
     }
 }
