2b5883dc54b8f5b377f2748a4b86f585038757cf
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-14 15:56:59 +0200

    Added Exercise One : Reverse a Given String

---
+++ Session-05/Session-05/ExerciseOne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class ExerciseOne
+    {
+        public string ReverseString(String inputString)
+        {
+            if (inputString == null)
+            {
+                return string.Empty;
+            }
+            string reversedString = string.Empty;
+            for (int i = inputString.Length - 1; i >= 0; i--)
+            {
+                reversedString += inputString[i];
+                
+            }
+            return reversedString;
+
+        }
+    }
+}
---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,19 @@
+﻿using Session_05;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 1 
+        Console.WriteLine("Please enter a string to be reversed: ");
+        string input = Console.ReadLine();
+
+        ExerciseOne reverser = new ExerciseOne();
+        ;
+        Console.WriteLine("Reversed String : {0} ", reverser.ReverseString(input));
+        Console.ReadLine();
+
+
+        
+    }
+}
