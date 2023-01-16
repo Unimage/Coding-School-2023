2789d9916da78cc5d9c141a2a264f0ad75f71cc7
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-16 15:45:50 +0200

    Added student class , methods and constructos

---
+++ Session-06/Session-06/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06
+{
+    internal class Student : Person
+    {
+        public int RegistrationNumber { get; set; }
+        public Course[] Courses;
+
+
+        public Student() 
+        {
+            Courses = new Course[20];
+
+        }
+        public Student(Guid id , string name,int registrationNumber ):base(id,name)
+        {
+            RegistrationNumber = registrationNumber;
+        }
+        public Student(Guid id, string name, int registrationNumber, Course[] courses) : base(id, name)
+        {
+            RegistrationNumber = registrationNumber;
+            Courses = new Course[20];
+        }
+
+
+        //TODO: Implement methods Attend and WriteExam
+        public void Attend(Course course,DateTime datetime)
+        {
+            
+        }
+        public void WriteExam(Course course, DateTime datetime)
+        {
+
+        }
+
+    }
+}
