2bf5c4b828ee5fb3d0421c90384ca08c2bd2d860
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-16 14:48:38 +0200

    Created Scedule class and added constructors.

---
+++ Session-06/Session-06/Schedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06
+{
+    internal class Schedule
+    {
+        public Guid ID { get; }
+        public Guid CourseID { get; set; }
+        public Guid ProfessorID { get; set; }
+        public DateTime Callendar { get; set; }
+
+        public Schedule()
+        {
+        }
+
+        public Schedule(Guid id)
+        {
+            ID = id;
+        }
+
+        public Schedule(Guid id, Guid courseID, Guid professorID)
+        {
+            CourseID = courseID;
+            ProfessorID = professorID;
+            ID= id;
+        }
+
+        //TODO: Add missing constructors / remove uneeded.
+    }
+}
