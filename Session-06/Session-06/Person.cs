3e22a27a8e08c1c5e727ac128e17ae19e47f73aa
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-16 14:59:41 +0200

    Added Person class , constructors and 2 unfinished methods (Get,Set)

---
+++ Session-06/Session-06/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06
+{
+    internal class Person
+    {
+        public Guid ID { get; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+        public Person(Guid id)
+        {
+            ID = id;
+        }
+        public Person(Guid id, string name)
+        { 
+            ID = id;
+            Name = name;
+        }
+        public Person(string name , int age)
+        {
+            Name= name;
+            Age = age;
+        }
+
+
+        public void GetName()
+        {
+            //TODO:change type to string and implement method.
+        }
+        public void SetName(string name)
+        {
+            //TODO:implemment method.
+        }
+    }
+}
