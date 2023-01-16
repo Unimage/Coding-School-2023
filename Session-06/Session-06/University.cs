c0f2ccf5b2c9cb339d48899a8769e984a4f2d87b
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-16 15:58:34 +0200

    Added University class , constructors and methods.

---
+++ Session-06/Session-06/University.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06
+{
+    internal class University : Institute
+    {
+        Student[] Students;
+        Course[] Courses;
+        Grade[] Grades;
+        Schedule[] ScheduledCourses;
+
+        public University()
+        {
+            Students = new Student[100];
+            Courses = new Course[20];
+            Grades = new Grade[200]; //courses * students
+            ScheduledCourses = new Schedule[60];
+        }  
+        public University(Guid id, string name, int yearsInService):base(id,name, yearsInService)
+        {
+            Students = new Student[100];
+            Courses = new Course[20];
+            Grades = new Grade[200]; //courses * students
+            ScheduledCourses = new Schedule[60];
+        }
+
+        //added another one for importing a whole university with all data
+        public University(Guid id, string name, int yearsInService, Student[] students, Course[] courses, Grade[] grades, Schedule[] scheduledCourses) : base(id, name, yearsInService)
+        {
+            Students = students;
+            Courses = courses;
+            Grades = grades;
+            ScheduledCourses = scheduledCourses;
+        }
+        public void GetStudents()
+        {
+
+        }
+        public void GetCourses()
+        {
+
+        }
+        public void GetGrades() 
+        {
+
+        }
+        public void SetSchedule(Guid coursID, Guid professorID, DateTime datetime)
+        {
+
+        }
+    }
+}
