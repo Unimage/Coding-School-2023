cfabc7314b7e2928e11dea10a7243dd2e5c5ab83
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-17 14:29:35 +0200

    Create project and added Enumerations.cs

---
+++ Session-07/Session-07/Enumerations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07 {
+    internal enum ActionEnumeration {
+        Convert,
+        Uppercase,
+        Reverse
+    }
+}
