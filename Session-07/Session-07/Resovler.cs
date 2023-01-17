6a8810a5c185139d07cd428b6cc2a0bd33b5bc68
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-17 15:36:26 +0200

    Created Resovler class with virtual methods to be inherited and implemeted by ActionResovler.

---
+++ Session-07/Session-07/Resovler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07 {
+    internal class Resovler {
+        public virtual void LogEventMessage(string description, DateTime timeStamp) { }
+        public virtual void LogEventMessage(Guid requestID, string requestIn, string requestOut, ActionEnum action, DateTime timStamp) { }
+        public virtual void LogEventError(Guid requestID, string requestIn, ActionEnum action, DateTime timStamp) { }
+    }
+}
