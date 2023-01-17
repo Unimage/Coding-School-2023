84f787c33aa4bd8a1e7794ba218b2157143b5868
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-17 14:36:51 +0200

    Added Message.cs

---
+++ Session-07/Session-07/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07 {
+    internal class Message {
+        public Guid ID { get; }
+        public DateTime TimeStamp { get; set; }
+        public string Content { get; set; } //changed name of Message:String
+
+        public Message() {
+            ID = Guid.NewGuid();
+        }
+    }
+}
