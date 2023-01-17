885fa99ded5e57cb68191d8624c028d8761694ae
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-17 15:03:39 +0200

    Added ActionResponce class with a basic constructor.

---
+++ Session-07/Session-07/ActionResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07 {
+    internal class ActionResponse {
+        public Guid RequestID { get; set; }
+        public Guid ResponseID { get; set; }
+        public string Output { get; set; }
+
+        public ActionResponse() {
+        ResponseID = Guid.NewGuid();    
+        }
+    }
+}
