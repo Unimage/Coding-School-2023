f25f2a1a56b4788b61aa11fb1c3a93fd2585e843
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-19 17:03:45 +0200

    Handled Devide by zero exception

--- Session-09/Calculation/Divition.cs
+++ Session-09/Calculation/Divition.cs
@@ -9,10 +9,18 @@ namespace Calculation {
     internal class Divition:Operation {
         public override string Calculate(Expression ex) {
 
-            decimal varOne; decimal.TryParse(ex.Values[0], out varOne);
-            decimal varTwo; decimal.TryParse(ex.Values[1], out varTwo);
-            decimal result = varOne / varTwo;
-            return result.ToString();
+            try {
+
+                decimal varOne; decimal.TryParse(ex.Values[0], out varOne);
+                decimal varTwo; decimal.TryParse(ex.Values[1], out varTwo);
+                decimal result = varOne / varTwo;
+                return result.ToString();
+
+            }
+            catch(DivideByZeroException) {
+                return "Cannot devide by zero";
+            }
+
         }
     }
 }
