dbb01b2fb37c11205693d63e2e0ccdeb25990021
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-19 17:19:27 +0200

    Removed depricated enum types.

--- Session-09/Calculation/Enumerations.cs
+++ Session-09/Calculation/Enumerations.cs
@@ -1,22 +1,4 @@
 ﻿namespace Calculation {
-    public enum NumbersEnum {
-        Zero = 0,
-        One = 1,
-        Two = 2,
-        Three = 3,
-        Four = 4,
-        Five = 5,
-        Six = 6,
-        Sevenv = 7,
-        Eight = 8,
-        Nine = 9,
-    }
-    public enum SymbolsEnum {
-
-        Comma ='.',
-        Equals='='
-    }
-
     public enum OperationsEnum {
         Plus = '+',
         Minus ='-',
@@ -25,5 +7,4 @@
         Power = '^',
         SquareRoot = 's'
     }
-    
 }
