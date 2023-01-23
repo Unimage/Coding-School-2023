ea106a52ea571b56fde8286d0742450f593c1527
Georgios Zacharidis <zageo@outlook.com>
2023-01-23 22:47:57 +0200

    Blueprint for MonthlyLedger added on class

--- Session-11/Libs/MonthlyLedger.cs
+++ Session-11/Libs/MonthlyLedger.cs
@@ -9,6 +9,21 @@ namespace Libs {
     //TODO:Discuss and implement ledger 
     public class MonthlyLedger {
 
+        public int Year  { get; set; }
+        public int Month { get; set; }
+
+        public List<Double> Income { get; set; } 
+        public List<Double> Expense { get; set; }
+
+        public Double Total { get; set; }    // Income - Expense 
+
+
         
+
+
+
+
+
+
     }
 }
