3780aa34e745a50da34e87f1464b1cc88af3e81d
Georgios Zacharidis <zageo@outlook.com>
2023-01-23 15:09:12 +0200

    test txt added

--- Session-11/Session-11/Form1.Designer.cs
+++ Session-11/Session-11/Form1.Designer.cs
@@ -29,7 +29,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1039, 775);
+            this.ClientSize = new System.Drawing.Size(1016, 790);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
