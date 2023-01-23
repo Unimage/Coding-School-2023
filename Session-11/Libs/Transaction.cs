bb0d8183391b0f28fb09458ee91291702e08fa03
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-01-23 17:26:58 +0200

    Transaction Class

---
+++ Session-11/Libs/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libs
+{
+    public class Transaction
+    {
+        public Guid ID { get; set; }
+        public DateTime Date { get; set; }
+        public Guid CustomerID { get; set; }
+        public Guid EmployeeID { get; set; }
+        public Enum PaymentMethod { get; set; }
+    
+        public Transaction()
+        {
+            ID= Guid.NewGuid();
+        }
+        public Transaction( DateTime date, Guid customerID, Guid employeeID)
+        {
+            Date = date;
+            CustomerID = customerID;
+            EmployeeID = employeeID;
+        }   
+    }
+}
