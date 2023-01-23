fd5b3f1b90c82b61609aa3b12f24a434f342f717
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-01-23 16:59:35 +0200

    TransactionLine class

--- Session-11/Libs/Class1.cs
+++ Session-11/Libs/Class1.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Libs
 {
-    internal class Class1
+    public class TransactionLine
     {
+        public Guid ID { get; set; }
+        public Guid ProductID { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public double Discount { get; set; }
+        public double TotalPrice { get; set; }
+
+        public TransactionLine ()
+        { 
+        }
+        public TransactionLine (Guid id)
+        {
+            ID = id;
+        }
+        public TransactionLine(Guid iD, Guid productID, int quantity, double price, double discount, double totalPrice)
+        {
+            ProductID = productID;
+            Quantity = quantity;
+            Price = price;
+            Discount = discount;
+            TotalPrice = totalPrice;
+        }
     }
 }
+
+
+
+
+
+
+    
+   
+
