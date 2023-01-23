e7c54e5c36b87797c8299863c0b02c66cf2ece55
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-23 14:57:47 +0200

    added enums

--- Session-11/Libs/Enumerations.cs
+++ Session-11/Libs/Enumerations.cs
@@ -1,7 +1,15 @@
 ﻿namespace Libs {
     public class Enumerations {
         public enum ProductType {
-
+            Coffee,
+            Beverages,
+            Food
+        }
+        public enum EmployeeType {
+            Manager,
+            Cashier,
+            Barista,
+            Waiter
         }
 
         //testing push
