721d77d34403e3a6971a92f47552fcf507245faf
Georgios Zacharidis <zageo@outlook.com>
2023-01-24 12:51:19 +0200

    ExceptionLogger class created

---
+++ Session-11/Libs/ExceptionLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libs
+{
+    // append error messages received through try-catch and log them with append to a file "exceptions.txt"
+    internal class ExceptionLogger
+    {
+    }
+}
