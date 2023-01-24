1f84be13dc78f35836cc7b629c069cad112755c9
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-24 13:32:10 +0200

    Added todo for nikos and yanis

--- Session-11/Session-11/Form1.cs
+++ Session-11/Session-11/Form1.cs
@@ -2,7 +2,6 @@ namespace Session_11 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
-            //this is another comment i made
         }
     }
 }
