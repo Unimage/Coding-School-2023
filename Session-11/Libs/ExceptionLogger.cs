345a23840b19ded2f20dd6f0f654ad3d742af11b
Georgios Zacharidis <zageo@outlook.com>
2023-01-24 13:02:09 +0200

    Changed the property for MessageTime

--- Session-11/Libs/ExceptionLogger.cs
+++ Session-11/Libs/ExceptionLogger.cs
@@ -9,5 +9,43 @@ namespace Libs
     // append error messages received through try-catch and log them with append to a file "exceptions.txt"
     internal class ExceptionLogger
     {
+        public string  ErrorMessage { get; set; }
+
+        public ExceptionLogger() { }
+
+
+        public void Log(string message) {
+
+
+            string logfile = @"exceptions.txt";
+
+            //the file does NOT exist 
+            if(!File.Exists(logfile)) 
+            {
+                using (StreamWriter streamWriter = new StreamWriter(logfile))
+                {
+                    streamWriter.WriteLine("Error Logger for Coffee Shop");
+                }
+
+            }
+            else
+            {
+                using (StreamWriter streamWriter = File.AppendText(logfile){
+
+                    
+                    streamWriter.WriteLine(System.DateTime.Today);
+                    streamWriter.WriteLine(message);
+                    streamWriter.Close();
+
+                }
+                {
+                    
+                }
+            }
+
+
+
+        
+        }
     }
 }
