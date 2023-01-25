394ebc3904e9a70f60f6528e9aa8c89e699efaa2
Georgios Zacharidis <zageo@outlook.com>
2023-01-25 09:51:08 +0200

    Appends to JSON file

--- Session-11/Libs/LedgerFileHelper.cs
+++ Session-11/Libs/LedgerFileHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,8 +22,25 @@ namespace Libs
         }
 
 
-        public void  AppendToLedger()
+        public void  AppendToLedger(decimal income , decimal expenses , decimal total )
         {
+            try
+            {
+                string fileName = ($"{this.Year}-{this.Month}.dat");
+                using (var fileStream = new FileStream(fileName, FileMode.Append))
+                {
+                    var bFormatter = new BinaryFormatter();
+                    bFormatter.Serialize(fileStream, objectToSerialize);
+                }
+            } catch (Exception e) 
+            { 
+            
+                    ExceptionLogger exceptionLogger = new ExceptionLogger(System.DateTime.Now);
+                    exceptionLogger.Log(e.ToString());
+
+
+                    
+            }
 
         }
 
