5952fdd512d7a0efc3bc0efcaa2b495c5909d881
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-26 00:14:28 +0200

    Add grid control and grid view to product category form

--- Session-11/Session-11/ProductCategoryF.Designer.cs
+++ Session-11/Session-11/ProductCategoryF.Designer.cs
@@ -23,12 +23,44 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.grdProductCategory = new DevExpress.XtraGrid.GridControl();
+            this.grvProductCategory = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdProductCategory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvProductCategory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdProductCategory
+            // 
+            this.grdProductCategory.Location = new System.Drawing.Point(44, 38);
+            this.grdProductCategory.MainView = this.grvProductCategory;
+            this.grdProductCategory.Name = "grdProductCategory";
+            this.grdProductCategory.Size = new System.Drawing.Size(687, 300);
+            this.grdProductCategory.TabIndex = 0;
+            this.grdProductCategory.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.grvProductCategory});
+            // 
+            // grvProductCategory
+            // 
+            this.grvProductCategory.GridControl = this.grdProductCategory;
+            this.grvProductCategory.Name = "grvProductCategory";
+            // 
+            // ProductCategoryF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdProductCategory);
+            this.Name = "ProductCategoryF";
             this.Text = "ProductCategoryF";
+            ((System.ComponentModel.ISupportInitialize)(this.grdProductCategory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvProductCategory)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraGrid.GridControl grdProductCategory;
+        private DevExpress.XtraGrid.Views.Grid.GridView grvProductCategory;
     }
 }
