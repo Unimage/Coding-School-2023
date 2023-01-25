0d0135cd320592e86fed87b9fd3acec923360f84
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-01-25 19:04:57 +0200

    Clear Form to start wokring on ui by Unimage

--- Session-11/Session-11/Form1.Designer.cs
+++ Session-11/Session-11/Form1.Designer.cs
@@ -23,147 +23,19 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
-            this.bsProducts = new System.Windows.Forms.BindingSource(this.components);
-            this.grvProducts = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colQuantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colbtnadd = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.btnNewOrder = new System.Windows.Forms.Button();
-            this.grvTransaction = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.bsProducts)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grvProducts)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grvTransaction)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(174, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Product List";
-            // 
-            // grvProducts
-            // 
-            this.grvProducts.AllowUserToDeleteRows = false;
-            this.grvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grvProducts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.colName,
-            this.colCode,
-            this.colQuantity,
-            this.colPrice,
-            this.colbtnadd});
-            this.grvProducts.Location = new System.Drawing.Point(12, 37);
-            this.grvProducts.Name = "grvProducts";
-            this.grvProducts.RowTemplate.Height = 25;
-            this.grvProducts.Size = new System.Drawing.Size(395, 454);
-            this.grvProducts.TabIndex = 2;
-            this.grvProducts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grvProducts_CellContentClick);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(427, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // colName
-            // 
-            this.colName.HeaderText = "Name";
-            this.colName.Name = "colName";
-            this.colName.ReadOnly = true;
-            this.colName.Width = 90;
-            // 
-            // colCode
-            // 
-            this.colCode.HeaderText = "Code";
-            this.colCode.Name = "colCode";
-            this.colCode.ReadOnly = true;
-            this.colCode.Width = 50;
-            // 
-            // colQuantity
-            // 
-            this.colQuantity.FillWeight = 60F;
-            this.colQuantity.HeaderText = "Quantity";
-            this.colQuantity.Name = "colQuantity";
-            this.colQuantity.Width = 60;
-            // 
-            // colPrice
-            // 
-            this.colPrice.HeaderText = "Price";
-            this.colPrice.Name = "colPrice";
-            this.colPrice.ReadOnly = true;
-            this.colPrice.Width = 50;
-            // 
-            // colbtnadd
-            // 
-            this.colbtnadd.HeaderText = "Add";
-            this.colbtnadd.Name = "colbtnadd";
-            this.colbtnadd.ReadOnly = true;
-            this.colbtnadd.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.colbtnadd.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.colbtnadd.Text = "Add Cart";
-            this.colbtnadd.UseColumnTextForButtonValue = true;
-            // 
-            // btnNewOrder
-            // 
-            this.btnNewOrder.Location = new System.Drawing.Point(673, 50);
-            this.btnNewOrder.Name = "btnNewOrder";
-            this.btnNewOrder.Size = new System.Drawing.Size(75, 23);
-            this.btnNewOrder.TabIndex = 4;
-            this.btnNewOrder.Text = "New Order";
-            this.btnNewOrder.UseVisualStyleBackColor = true;
-            this.btnNewOrder.Click += new System.EventHandler(this.btnNewOrder_Click);
-            // 
-            // grvTransaction
-            // 
-            this.grvTransaction.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grvTransaction.Location = new System.Drawing.Point(808, 37);
-            this.grvTransaction.Name = "grvTransaction";
-            this.grvTransaction.RowTemplate.Height = 25;
-            this.grvTransaction.Size = new System.Drawing.Size(354, 215);
-            this.grvTransaction.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1598, 728);
-            this.Controls.Add(this.grvTransaction);
-            this.Controls.Add(this.btnNewOrder);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.grvProducts);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.bsProducts)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grvProducts)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grvTransaction)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private Label label1;
-        private BindingSource bsProducts;
-        private DataGridView grvProducts;
-        private Button button1;
-        private DataGridViewTextBoxColumn colName;
-        private DataGridViewTextBoxColumn colCode;
-        private DataGridViewTextBoxColumn colQuantity;
-        private DataGridViewTextBoxColumn colPrice;
-        private DataGridViewButtonColumn colbtnadd;
-        private Button btnNewOrder;
-        private DataGridView grvTransaction;
     }
 }
--- Session-11/Session-11/Form1.cs
+++ Session-11/Session-11/Form1.cs
@@ -29,47 +29,6 @@ namespace Session_11 {
 
         private void load() {
             CoffeeShop.Init();
-            bsProducts.DataSource = CoffeeShop.Products; 
-
         }
-
-        public void SetControlProperties(){
-            grvProducts.AutoGenerateColumns = false;
-            grvProducts.DataSource = bsProducts;
-            grvProducts.Columns[0].DataPropertyName = "Description";
-            grvProducts.Columns[1].DataPropertyName = "Code";
-            grvProducts.Columns[3].DataPropertyName = "Price";
-
-
-
-        }
-
-        private void button1_Click(object sender, EventArgs e) {
-            load();
-            SetControlProperties();
-        }
-
-        private void grvProducts_CellContentClick(object sender, DataGridViewCellEventArgs e) {
-            if (e.RowIndex >= 0 && e.ColumnIndex == grvProducts.Columns["colbtnadd"].Index) {
-                DataGridViewRow row = grvProducts.Rows[e.RowIndex];
-
-                translinetobeadded = new();
-                translinetobeadded.Quantity = Int32.Parse(row.Cells["colQuantity"].Value.ToString()) ;
-                translinetobeadded.Price = Decimal.Parse(row.Cells["colPrice"].Value.ToString());
-                Transaction.AddTransactionLines(translinetobeadded);
-
-            }
-
-
-
-        }
-
-        private void btnNewOrder_Click(object sender, EventArgs e) {
-            tmp = true;
-            
-            grvTransaction.DataSource = Transaction._transaction.TransactionLines;
-
-
-        }
-    }
+    }        
 }
