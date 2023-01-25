4272990161f124fc133167212b1ba392f242bdc5
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-26 01:57:06 +0200

    Add product form

---
+++ Session-11/Session-11/ProductF.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Session_11 {
+    partial class ProductF {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.grdProduct = new DevExpress.XtraGrid.GridControl();
+            this.grvProduct = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grdProduct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdProduct
+            // 
+            this.grdProduct.Location = new System.Drawing.Point(12, 12);
+            this.grdProduct.MainView = this.grvProduct;
+            this.grdProduct.Name = "grdProduct";
+            this.grdProduct.Size = new System.Drawing.Size(776, 292);
+            this.grdProduct.TabIndex = 0;
+            this.grdProduct.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.grvProduct});
+            // 
+            // grvProduct
+            // 
+            this.grvProduct.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1});
+            this.grvProduct.GridControl = this.grdProduct;
+            this.grvProduct.Name = "grvProduct";
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "gridColumn1";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            // 
+            // ProductF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdProduct);
+            this.Name = "ProductF";
+            this.Text = "ProductF";
+            ((System.ComponentModel.ISupportInitialize)(this.grdProduct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grvProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl grdProduct;
+        private DevExpress.XtraGrid.Views.Grid.GridView grvProduct;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+    }
+}
