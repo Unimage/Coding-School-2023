917f45906430f5ee5d6936a9e11cdec7c47dd777
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-03 09:35:59 +0200

    Created a test aplication for basic api example.

---
+++ Session-19/Session-19/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Session_19.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase {
+
+        [HttpGet(Name = "GetPersonName")]
+        public Person Get() {
+
+            Person person = new Person();
+            person.FName = "Stratos";
+            person.age = 25;
+            person.LName = "Chalkopiadis";
+
+            return person;
+        }
+    }
+}
---
+++ Session-19/Session-19/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace Session_19 {
+    public class Person {
+        public string? FName { get; set; }
+        public string? LName { get; set; }
+        public int age { get; set; }
+    }
+}
