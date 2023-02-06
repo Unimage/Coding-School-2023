cc94c0f23bbbafaf5a6a8baf91556f6d0f4846bc
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-06 09:22:11 +0200

    Committed unsaved changes on TransLineRepo.cs

---
+++ Session-16/CoffeeShop.Orm/Repositories/TransactionLineRepo.cs
@@ -0,0 +1,50 @@
+﻿using CoffeeShop.Model;
+using CoffeeShop.Orm.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Orm.Repositories {
+    internal class TransactionLineRepo : IEntityRepo<TransactionLine> {
+        public void Create(TransactionLine entity) {
+            using var context = new ApplicationDBContext();
+            context.TransactionLines.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(Guid id) {
+            using var context = new ApplicationDBContext();
+            var foundTransactionLine = context.TransactionLines.SingleOrDefault(TransactionLine => TransactionLine.ID == id);
+            if (foundTransactionLine is null)
+                return;
+
+            context.TransactionLines.Remove(foundTransactionLine);
+            context.SaveChanges();
+        }
+
+        public List<TransactionLine> GetAll() {
+            using var context = new ApplicationDBContext();
+            return context.TransactionLines.ToList();
+        }
+
+        public TransactionLine? GetById(Guid id) {
+            using var context = new ApplicationDBContext();
+            return context.TransactionLines.Where(TransactionLine => TransactionLine.ID == id).SingleOrDefault();
+        }
+
+        public void Update(Guid id, TransactionLine entity) {
+            using var context = new ApplicationDBContext();
+            var foundTransactionLine = context.TransactionLines.SingleOrDefault(TransactionLine => TransactionLine.ID == id);
+            if (foundTransactionLine is null)
+                return;
+
+            foundTransactionLine.Quantity = entity.Quantity;
+            foundTransactionLine.TotalPrice = entity.TotalPrice;
+            foundTransactionLine.Product = entity.Product;
+            foundTransactionLine.ProductID = entity.ProductID;
+            context.SaveChanges();
+        }
+    }
+}
