5b61cc026eadb5f3a4298a7a68a109530a2fb845
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-09 15:08:07 +0200

    Added ProductCategory View  for index inserted productCategory Dto

--- Session-23/CoffeeShop.MVC/Controllers/ProductCategoryController.cs
+++ Session-23/CoffeeShop.MVC/Controllers/ProductCategoryController.cs
@@ -1,16 +1,39 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CoffeeShop.EF.Repositories;
+using CoffeeShop.Model;
+using CoffeeShop.MVC.Models.ProductCategory;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoffeeShop.MVC.Controllers {
     public class ProductCategoryController : Controller {
+        private IEntityRepo<ProductCategory> _productCategoryRepo;
+        public ProductCategoryController(IEntityRepo<ProductCategory> productCategoryRepo) {
+            _productCategoryRepo = productCategoryRepo;
+        }
+
         // GET: ProductCategoryController
         public ActionResult Index() {
-            return View();
+            var tmp = _productCategoryRepo.GetAll();
+            var productCategories = tmp.ToList();
+            return View(model: productCategories);
         }
 
         // GET: ProductCategoryController/Details/5
-        public ActionResult Details(int id) {
-            return View();
+        public ActionResult Details(int? id) {
+            if(id == null) { return NotFound(); }
+            var prodCatLookup = _productCategoryRepo.GetById(id.Value);
+            if (prodCatLookup == null) {
+                return NotFound();
+            }
+            var viewProductCategory = new ProductCategoryDto {
+                Id = prodCatLookup.Id,
+                Code = prodCatLookup.Code,
+                Description = prodCatLookup.Description,
+                ProductType = prodCatLookup.ProductType,
+                Products = prodCatLookup.Products.ToList()
+            };
+
+            return View(model:viewProductCategory);
         }
 
         // GET: ProductCategoryController/Create
