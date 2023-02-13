3fc0eb5804dba6a73350526f2efa8747792fcc03
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-13 05:01:02 +0200

    Added employee CheckLimits Staff to comply with project requirments. Its commented out on employee controller so i it works for now. Need to add  mock data to comply initially or fix it otherwise

--- Session-23/CoffeeShop.MVC/Controllers/EmployeeController.cs
+++ Session-23/CoffeeShop.MVC/Controllers/EmployeeController.cs
@@ -1,8 +1,10 @@
 ﻿using CoffeeShop.EF.Repositories;
 using CoffeeShop.Model;
+using CoffeeShop.Model.Enums;
 using CoffeeShop.MVC.Models.Employee;
 using CoffeeShop.MVC.Models.ProductCategory;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Pkcs;
 
 namespace CoffeeShop.MVC.Controllers {
     public class EmployeeController : Controller {
@@ -17,7 +19,7 @@ namespace CoffeeShop.MVC.Controllers {
         public ActionResult Index() {
             var emp = _employeeRepo.GetAll();
             var employees = emp.ToList();
-            return View(model:employees);
+            return View(model: employees);
         }
 
         // GET: EmployeeController/Details/5
@@ -30,16 +32,16 @@ namespace CoffeeShop.MVC.Controllers {
                 return NotFound();
             }
             var viewEmployee = new EmployeeDetailsDto {
-                EmployeeType= employee.EmployeeType,
-                Id= employee.Id,
-                Name= employee.Name,
-                Surname= employee.Surname,
-                SalaryPerMonth= employee.SalaryPerMonth
-            
+                EmployeeType = employee.EmployeeType,
+                Id = employee.Id,
+                Name = employee.Name,
+                Surname = employee.Surname,
+                SalaryPerMonth = employee.SalaryPerMonth
+
             };
             var transList = _transactionRepo.GetAll().ToList();
-            foreach(var tr in transList) {
-                if(tr.EmployeeId == viewEmployee.Id) {
+            foreach (var tr in transList) {
+                if (tr.EmployeeId == viewEmployee.Id) {
                     viewEmployee.Transactions.Add(tr);
                 }
             }
@@ -57,9 +59,11 @@ namespace CoffeeShop.MVC.Controllers {
         [ValidateAntiForgeryToken]
         public ActionResult Create(EmployeeCreateDto employee) {
             if (!ModelState.IsValid) {
+                //  if (!CheckStaffLimits(employee)) { return View(); }
                 return View();
+
             }
-            var dbEmployee = new Employee(employee.Name,employee.Surname,employee.SalaryPerMonth,employee.EmployeeType);
+            var dbEmployee = new Employee(employee.Name, employee.Surname, employee.SalaryPerMonth, employee.EmployeeType);
             try {
                 _employeeRepo.Create(dbEmployee);
                 return RedirectToAction(nameof(Index));
@@ -81,16 +85,18 @@ namespace CoffeeShop.MVC.Controllers {
             var editEmployee = new EmployeeEditDto {
                 Id = dbEmployee.Id
             };
-       
+
             return View(model: editEmployee);
         }
-    
+
 
         // POST: EmployeeController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, EmployeeEditDto employee) {
             if (!ModelState.IsValid) {
+
+                // if (!CheckStaffLimits(employee)) { return View(); }
                 return View();
             }
             var dbEmployee = _employeeRepo.GetById(id);
@@ -111,10 +117,11 @@ namespace CoffeeShop.MVC.Controllers {
             if (dbEmployee == null) {
                 return NotFound();
             }
+            
             var deleteEmployee = new EmployeeDeleteDto {
                 Id = dbEmployee.Id,
-                Name= dbEmployee.Name,
-                Surname= dbEmployee.Surname
+                Name = dbEmployee.Name,
+                Surname = dbEmployee.Surname
             };
             return View(model: deleteEmployee);
         }
@@ -123,8 +130,67 @@ namespace CoffeeShop.MVC.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection) {
+           // if (!CheckStaffLimits(collection)) { return View(); }
             _employeeRepo.Delete(id);
             return RedirectToAction(nameof(Index));
         }
+        #region Handlers
+        public bool CheckStaffLimits(object employeeToBeHandled) {
+            var Employees = _employeeRepo.GetAll().ToList();
+            if (employeeToBeHandled.GetType() == typeof(EmployeeCreateDto)) {
+                var create = employeeToBeHandled as EmployeeCreateDto;
+                Employee inserted = new Employee(create.Name, create.Surname, create.SalaryPerMonth, create.EmployeeType);
+                Employees.Add(inserted);
+            }
+            else if (employeeToBeHandled.GetType() == typeof(EmployeeDeleteDto)) {
+                var delete = employeeToBeHandled as EmployeeDeleteDto;
+                Employee deleted = new Employee(delete.Name, delete.Surname, 0, 0);
+                Employees.Remove(deleted);
+            }
+            else if (employeeToBeHandled.GetType() == typeof(EmployeeEditDto)) {
+                var update = employeeToBeHandled as EmployeeEditDto;
+                Employee updated = new Employee(update.Name, update.Surname, update.SalaryPerMonth, update.EmployeeType);
+                foreach (var emp in Employees)
+                    if (emp.Id == update.Id) {
+                        emp.Surname = update.Surname;
+                        emp.Name = update.Name;
+                        emp.SalaryPerMonth = update.SalaryPerMonth;
+                        emp.EmployeeType = update.EmployeeType;
+                    }
+            }
+            bool ok = true;
+            int managers = 0;
+            int cashiers = 0;
+            int baristas = 0;
+            int waiters = 0;
+            foreach (var employee in Employees) {
+                switch (employee.EmployeeType) {
+                    case EmployeeType.Manager:
+                        managers++;
+                        break;
+                    case EmployeeType.Cashier:
+                        cashiers++;
+                        break;
+                    case EmployeeType.Barista:
+                        baristas++;
+                        break;
+                    case EmployeeType.Waiter:
+                        waiters++;
+                        break;
+                }
+            }
+            if (managers != 1)
+                ok= false;
+            ;
+            if (cashiers < 1 || cashiers > 2)
+                ok= false;
+            if (baristas < 1 || baristas > 2)
+                ok= false;
+            if (waiters < 1 || waiters > 2)
+                ok= false; ;
+            return ok;
+        }
     }
 }
+
+#endregion
