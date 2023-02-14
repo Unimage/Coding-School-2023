58facfa8c1319cd758b6187cdef0747343a0ad11
GaDAlex <alexaras1311@gmail.com>
2023-02-14 15:43:04 +0300

    add engineer/manager repo client/server/shared

--- Session-27/Session-27/Server/Controllers/ManagerController.cs
+++ Session-27/Session-27/Server/Controllers/ManagerController.cs
@@ -3,9 +3,56 @@
 namespace Session_27.Server.Controllers
 {
     [Route("[controller]")]
-    [ApiController]
-    public class ManagerController : Controller {
-    
+    [Controller]
+    public class ManagerController : ControllerBase {
+        private readonly IEntityRepo<Engineer> _managerRepo;
+
+        public ManagerController(IEntityRepo<Manager> managerRepo) {
+            _managerRepo = managerRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ManagerDto>> Get() {
+            var result = _managerRepo.GetAll();
+            return result.Select(manager => new ManagerDto {
+                Name = manager.Name,
+                Surname = manager.Surname,
+                SalaryPerMonth = manager.SalaryPetMonth
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ManagerDto> GetById(int id) {
+            var result = _managerRepo.GetById(id);
+            return new ManagerEditDto {
+                Id = id,
+                Name = manager.Name,
+                Surname = manager.Surname,
+                SalaryPerMonth = manager.SalaryPetMonth
+            };
+        }
+
+        [HttpPost]
+        public async Task Post(ManagerEditDto manager) {
+            var newManager = new Manager(manager.Name);
+            newManager.Surname = manager.Surname;
+            newManager.SalaryPetMonth = manager.SalaryPetMonth;
+            _managerRepo.Add(newManager);
+        }
+
+        [HttpPut]
+        public async Task Put(ManagerEditDto manager) {
+            var itemToUpdate = _managerRepo.GetById(manager.Id);
+            itemToUpdate.Name = manager.Name;
+            itemToUpdate.Surname = manager.Surname;
+            itemToUpdate.SalaryPetMonth = manager.SalaryPetMonth;
+            _managerRepo.Update(manager.Id, itemToUpdate);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id) {
+            _managerRepo.Delete(id);
+        }
     }
 }
 
---
+++ Session-27/Session-27/Shared/EngineerListDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_27.Shared
+{
+    internal class EngineerListDto {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public int SalaryPerMonth { get; set; }
+            public int ManagerId { get; set; }
+            public Manager Manager { get; set; } = null!;
+    }
+    }
+
