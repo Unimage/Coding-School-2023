b9e5e6d0359dc7f15a76259ad5960cb7003ed079
Anna Ilia <annailia9802@gmail.com>
2023-02-14 21:22:49 +0200

    rip

---
+++ Session-27/Session-27/Shared/CustomerListDto.cs
@@ -0,0 +1,23 @@
+﻿using Session_27.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_27.Shared
+{
+    public class CustomerListDto
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Phone { get; set; }
+        public string Tin { get; set; }
+
+        // Relations
+        public List<Transaction> Transactions { get; set; } = new();
+
+    }
+}
