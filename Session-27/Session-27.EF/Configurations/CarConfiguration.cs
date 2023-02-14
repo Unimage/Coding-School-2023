9d6489d5a74c76cc6b6963318745698db0c7c43b
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-14 15:37:32 +0200

    Added Model and EF / Still missing Repositories

---
+++ Session-27/Session-27.EF/Configurations/CarConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Session_27.Model;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Session_27.Model.EF.Configurations
+{
+    public class CarConfiguration : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            // Table Name
+            builder.ToTable("Cars");
+
+            // Primary Key
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id).ValueGeneratedOnAdd();
+
+            // Properties
+            builder.Property(t => t.Brand).HasMaxLength(30).IsRequired();
+            builder.Property(t => t.Model).HasMaxLength(50).IsRequired();
+            builder.Property(t => t.CarRegistrationNumber).HasMaxLength(10).IsRequired();
+
+            // Relations
+
+        }
+    }
+}
