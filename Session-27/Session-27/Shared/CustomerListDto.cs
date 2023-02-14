581d6a39d6b092d42d0a1f1ffc5ae7c6c04f003d
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-14 21:30:53 +0200

    pray?!

--- Session-27/Session-27/Shared/CustomerListDto.cs
+++ Session-27/Session-27/Shared/CustomerListDto.cs
@@ -9,7 +9,6 @@ namespace Session_27.Shared
 {
     public class CustomerListDto
     {
-
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
