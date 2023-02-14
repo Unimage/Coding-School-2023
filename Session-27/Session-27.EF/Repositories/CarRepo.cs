e7ba807b2424b1fc6f3978a74d0f21a5037fe9ac
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-14 16:20:58 +0200

    Added repos

---
+++ Session-27/Session-27.EF/Repositories/CarRepo.cs
@@ -0,0 +1,55 @@
+﻿using Session_27.EF.Repositories;
+using Session_27.Model.EF.Context;
+using Session_27.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Session_27.EF.Repositories {
+    public class CarRepo : IEntityRepo<Car> {
+        public void Add(Car entity) {
+            using var context = new CarServiceCenterDbContext();
+                context.Add(entity);
+                context.SaveChanges();
+        }
+        public void Delete(int id) {
+            using var context = new CarServiceCenterDbContext();
+            var CarDb = context.Cars
+                .Where(car => car.Id == id)
+                .Include(car => car.Transactions)
+                .SingleOrDefault();
+            if (CarDb is null)
+                throw new KeyNotFoundException($"Given id '{id}' was not found");
+            context.Remove(CarDb);
+            context.SaveChanges();
+        }
+
+        public IList<Car> GetAll() {
+            using var context = new CarServiceCenterDbContext();
+            return context.Cars
+                .Include(car => car.Transactions)
+                .ToList();
+        }
+        public Car? GetById(int id) {
+            using var context = new CarServiceCenterDbContext();
+            return context.Cars
+                .Where(car => car.Id == id)
+                .Include(car => car.Transactions)
+                .SingleOrDefault(); 
+        }
+        public void Update(int id, Car entity) {
+            using var context = new CarServiceCenterDbContext();
+            var CarDb = context.Cars.Where(car => car.Id == id).SingleOrDefault();
+            if (CarDb is null)
+                throw new KeyNotFoundException($"Given id '{id}' was not found");
+            CarDb.Brand = entity.Brand;
+            CarDb.Model = entity.Model;
+            CarDb.CarRegistrationNumber = entity.CarRegistrationNumber;
+            context.SaveChanges();
+        }
+ 
+    }
+}
---
+++ Session-27/Session-27.EF/Repositories/CustomerRepo.cs
@@ -0,0 +1,56 @@
+﻿using Session_27.EF.Repositories;
+using Session_27.Model.EF.Context;
+using Session_27.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Session_27.EF.Repositories {
+    public class CustomerRepo : IEntityRepo<Customer> {
+        public void Add(Customer entity) {
+            using var context = new CarServiceCenterDbContext();
+                context.Add(entity);
+                context.SaveChanges();
+        }
+        public void Delete(int id) {
+            using var context = new CarServiceCenterDbContext();
+            var CustomerDb = context.Customers
+                .Where(customer => customer.Id == id)
+                .Include(customer => customer.Transactions)
+                .SingleOrDefault();
+            if (CustomerDb is null)
+                throw new KeyNotFoundException($"Given id '{id}' was not found");
+            context.Remove(CustomerDb);
+            context.SaveChanges();
+        }
+        public IList<Customer> GetAll() {
+            using var context = new CarServiceCenterDbContext();
+            return context.Customers
+                .Include(customer => customer.Transactions)
+                .ToList();
+        }
+        public Customer? GetById(int id) {
+            using var context = new CarServiceCenterDbContext();
+            return context.Customers
+                .Where(customer => customer.Id == id)
+                .Include(customer => customer.Transactions)
+                .SingleOrDefault();
+        }
+        public void Update(int id, Customer entity) {
+            using var context = new CarServiceCenterDbContext();
+            var CustomerDb = context.Customers
+                .Where(customer => customer.Id == id)
+                .Include(customer => customer.Transactions)
+                .SingleOrDefault();
+            if (CustomerDb is null) throw new KeyNotFoundException($"Given id '{id}' was not found");
+            CustomerDb.Name = entity.Name;
+            CustomerDb.Surname = entity.Surname;
+            CustomerDb.Phone = entity.Phone;
+            CustomerDb.Tin = entity.Tin;
+            context.SaveChanges();
+        }
+    }
+}
