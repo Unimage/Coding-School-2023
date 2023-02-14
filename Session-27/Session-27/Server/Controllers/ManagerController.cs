8007def949d152c49423dccd569a7963686a1598
GaDAlex <alexaras1311@gmail.com>
2023-02-14 13:29:11 +0300

    engineer-manager controller

---
+++ Session-27/Session-27/Server/Controllers/ManagerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Session_27.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ManagerController : Controller {
+    
+    }
+}
+
