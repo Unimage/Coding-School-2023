919bc4d498e9609bc064c44cd849c84867745db0
GaDAlex <alexaras1311@gmail.com>
2023-02-15 16:07:37 +0300

    BrandModelNum created

--- Session-27/Session-27/Shared/CustomerEditDto.cs
+++ Session-27/Session-27/Shared/CustomerEditDto.cs
@@ -12,8 +12,14 @@ namespace Session_27.Shared
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string FullName {
+            get {
+                return string.Format("{0} {1}", Name, Surname);
+            }
+        }
         public int Phone { get; set; }
         public string Tin { get; set; }
+
 
         public List<Transaction> Transactions { get; set; } = new List<Transaction>();
 
--- Session-27/Session-27/Shared/CustomerListDto.cs
+++ Session-27/Session-27/Shared/CustomerListDto.cs
@@ -12,6 +12,11 @@ namespace Session_27.Shared
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string FullName {
+            get {
+                return string.Format("{0} {1}", Name, Surname);
+            }
+        }
         public int Phone { get; set; }
         public string Tin { get; set; }
 
