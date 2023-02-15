4f538d4bdaf47ea63166c7aec9987207874f69c0
GaDAlex <alexaras1311@gmail.com>
2023-02-15 16:09:51 +0300

    transaction list dto

--- Session-27/Session-27/Shared/Transaction/TransactionListDto.cs
+++ Session-27/Session-27/Shared/Transaction/TransactionListDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Session_27.Shared.TransactionLine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace Session_27.Shared
 {
     public class TransactionListDto
     {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int CustomerId { get; set; }
+        public List<CustomerEditDto> Customers { get; set; } = new();
+        public int ManagerId { get; set; }
+        public List<ManagerEditDto> Managers { get; set; } = new();
+        public int CarId { get; set; }
+        public List<CarEditDto> Cars { get; set; } = new();
+        public List<TransactionLineEditDto> TransactionLines { get; set; } = new();
     }
 }
