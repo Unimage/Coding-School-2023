90ba57e2e3c7c69989e8f9be3e0981a6e0a8a825
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-02-15 18:51:01 +0200

    Transaction Controller complete

--- Session-27/Session-27/Server/Controllers/TransactionController.cs
+++ Session-27/Session-27/Server/Controllers/TransactionController.cs
@@ -1,21 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Session_27.EF.Repositories;
 using Session_27.Model;
 using Session_27.Shared;
 
-namespace Session_27.Server.Controllers {
+namespace Session_27.Server.Controllers
+{
     [Route("[controller]")]
     [ApiController]
-    public class TransactionController : ControllerBase {
+    public class TransactionController : ControllerBase
+    {
         private readonly IEntityRepo<Transaction> _transactionRepo;
         private readonly IEntityRepo<Customer> _customerRepo;
         private readonly IEntityRepo<Manager> _managerRepo;
         private readonly IEntityRepo<Car> _carRepo;
         private readonly IEntityRepo<ServiceTask> _serviceTaskRepo;
         private readonly IEntityRepo<Engineer> _engineerRepo;
+        //private readonly IEntityRepo<>
 
-        public TransactionController(IEntityRepo<Transaction> transactionRepo, IEntityRepo<Customer> customerRepo, IEntityRepo<Manager> managerRepo, IEntityRepo<Car> carRepo, IEntityRepo<ServiceTask> serviceTaskRepo, IEntityRepo<Engineer> engineerRepo) {
+        public TransactionController(IEntityRepo<Transaction> transactionRepo, IEntityRepo<Customer> customerRepo, IEntityRepo<Manager> managerRepo, IEntityRepo<Car> carRepo, IEntityRepo<ServiceTask> serviceTaskRepo, IEntityRepo<Engineer> engineerRepo)
+        {
 
             _transactionRepo = transactionRepo;
             _customerRepo = customerRepo;
@@ -28,10 +32,12 @@ namespace Session_27.Server.Controllers {
 
 
         // [HttpGet]
-        public async Task<IEnumerable<TransactionListDto>> Get() {
+        public async Task<IEnumerable<TransactionListDto>> Get()
+        {
             var trans = _transactionRepo.GetAll();
 
-            return trans.Select(trans => new TransactionListDto {
+            return trans.Select(trans => new TransactionListDto
+            {
                 Id = trans.Id,
                 Date = trans.Date,
                 TotalPrice = trans.TotalPrice,
@@ -65,30 +71,72 @@ namespace Session_27.Server.Controllers {
                 {
                     Id = car.Id,
                     Brand = car.Brand
-                }).ToList().FirstOrDefault(),
+                }).ToList().SingleOrDefault(),
                 Manager = managers.Select(manager => new Manager
                 {
                     Id = manager.Id,
                     Name = manager.Name
-                }).ToList().FirstOrDefault()
+                }).ToList().SingleOrDefault()
             };
+        }
+
+        [HttpPost]
+        public async Task Post(TransactionListDto transaction)
+        {
+            var newTransaction = new Transaction(transaction.TotalPrice);
+            newTransaction.Id = transaction.Id;
+            newTransaction.Date = transaction.Date;
+            newTransaction.TotalPrice = transaction.TotalPrice;
+            newTransaction.CustomerId = transaction.CustomerId;
+            newTransaction.ManagerId = transaction.ManagerId;
+            newTransaction.CarId = transaction.CarId;
+            //list of transaction lines
+            newTransaction.TransactionLines = transaction.TransactionLines; //TransactionLines = new List<TransactionLine>();
+
+            _transactionRepo.Add(newTransaction);
         }
 
 
 
+        [HttpPut]
+        public async Task Put(TransactionEditDto trans)
+        {
+            var transactionToUpdate = _transactionRepo.GetById(trans.Id);
 
+            //transactionToUpdate.CustomerId = trans.CustomerId;
+            //  transactionToUpdate.ManagerId = trans.ManagerId;
+            // transactionToUpdate.CarId = trans.CarId;
+            transactionToUpdate.TotalPrice = trans.TotalPrice;
+            transactionToUpdate.Date = trans.Date;
+            transactionToUpdate.TransactionLines = trans.TransactionLines.Select(transline => new TransactionLine(transline.Hours, transline.PricePerHour, transline.Price)
+            { 
+       
+            }
+            ).ToList();
+   
 
-        //[HttpGet("{id}")]
-
-
-
-
-        //[HttpPut]
-
+            _transactionRepo.Update(trans.Id, transactionToUpdate);
+        }
 
 
         //[HttpDelete("{id}")]
-
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                _transactionRepo.Delete(id);
+                return Ok();
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest("This todo cannot be deleted!");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return BadRequest($"Todo with id {id} not found!");
+            }
+        }
 
 
 
