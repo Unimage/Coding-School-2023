16e36bfa27090e2e494a07e36c87caeedd13ffbd
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-18 22:47:22 +0200

    Added Employee Config

---
+++ Session-30/FuelStation/FuelStation.EF/Configuration/EmployeeConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using FuelStation.Model;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EF.Configuration {
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee> {
+
+        public void Configure(EntityTypeBuilder<Employee> builder) {
+            //Table name 
+            builder.ToTable("Employees");
+
+            //Primary Key
+            builder.HasKey(employee => employee.ID);
+
+            builder.Property(employee => employee.Name).HasMaxLength(20).IsRequired();
+            builder.Property(employee => employee.Surname).HasMaxLength(20).IsRequired();
+            builder.Property(employee => employee.username).HasMaxLength(20);
+            builder.Property(employee => employee.password).HasMaxLength(256);
+            builder.Property(employee => employee.EmployeeType).HasMaxLength(20).IsRequired();
+            //TODO:Refactor and test so it displays name not just a value
+            //builder.Property(employee => employee.EmployeeType).HasConversion(employeeType => employeeType.ToString(), employeeType => (EmployeeTypeEnum)Enum.Parse(typeof(EmployeeTypeEnum), employeeType)).HasMaxLength(20);
+            builder.Property(employee => employee.HireDateStart).IsRequired();
+            builder.Property(employee => employee.HireDateEnd);
+            builder.Property(employee => employee.SallaryPerMonth).HasPrecision(7,2).IsRequired();
+        }
+    }
+}
