59e9ccfdcee92e369a387ab91d091e7a914dcaed
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-18 21:35:35 +0200

    Addeded Model Class Library and Created BaseEntity.

---
+++ Session-30/FuelStation/FuelStation.Model/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Model {
+    public class BaseEntity {
+        public Guid ID { get; }
+        public BaseEntity() { 
+            ID = Guid.NewGuid();
+        }
+
+    }
+}
