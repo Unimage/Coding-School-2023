ac0b7abec05caf530c19f4ab2890e253f12abb92
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-19 01:06:27 +0200

    Removed some default blazor junk and added a basic AuthorizationHanlder service to check for access.

---
+++ Session-30/FuelStation/FuelStation.Blazor/Shared/Services/AuthorizationHandler.cs
@@ -0,0 +1,50 @@
+﻿using FuelStation.Model;
+using FuelStation.Model.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Blazor.Shared.Services {
+
+    //basic WIP for authorization handling based on given analysis.
+    //TODO: Make it work 
+    public class AuthorizationHandler {
+
+        public bool HasAccessToItems(Employee employee) {
+            if (employee.EmployeeType == EmployeeType.Staff ||
+               employee.EmployeeType == EmployeeType.Manager) {
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasAccessToCustomers(Employee employee) {
+            if (employee.EmployeeType == EmployeeType.Manager ||
+                employee.EmployeeType == EmployeeType.Cashier) {
+                return true;
+            }
+            return false;
+        }
+        public bool HasAccessToEmployees(Employee employee) {
+            if (employee.EmployeeType == EmployeeType.Manager) {
+                return true;
+            }
+            return false;
+        }
+        public bool HasAccessToTransactions(Employee employee) {
+            if (employee.EmployeeType == EmployeeType.Manager ||
+                employee.EmployeeType == EmployeeType.Cashier) {
+                return true;
+            }
+            return false;
+        }
+        public bool HasAccessToLedger(Employee employee) {
+            if (employee.EmployeeType == EmployeeType.Manager) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
