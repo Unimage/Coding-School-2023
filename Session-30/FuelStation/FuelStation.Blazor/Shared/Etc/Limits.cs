37cae5b9dfbc0c8c864d681a0ec776155db7f1e8
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-19 11:03:20 +0200

    Added some DataLimits and a validator class for later use

---
+++ Session-30/FuelStation/FuelStation.Blazor/Shared/Etc/Limits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Blazor.Shared.Etc {
+    public class Limits {
+
+        #region Employee / Customer Shared Limits
+        private int _maxEmpNameSize = 20;
+        private int _maxSurnameSize = 20;
+        #endregion
+        #region Customer Exclusive Limits
+        private int _maxCardSize = 20;
+        private char _firstCardChar = 'A';
+        #endregion
+        #region Employee Exclusive Limits
+        private int _maxUsernameSize = 20;
+        private int _maxPasswordSize = 256;
+        private decimal _maxDecimal = 99999.99m;
+        #endregion
+        #region Item Limits
+        private int _maxCodeSize = 6;
+        private int _maxDesctiptionSize = 30;
+        #endregion
+    }
+}
---
+++ Session-30/FuelStation/FuelStation.Blazor/Shared/Services/DataValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Blazor.Shared.Services {
+
+    //Generic all in one Validator class containing all validation methods needed
+    //for server side validation 
+    //possibly some used in client-side / winforms 
+    //TODO:WIP
+    public class DataValidator {
+    }
+}
