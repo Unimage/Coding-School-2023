0995b353d4f96bcd416fbda361115b06a07b01ef
Stratos Chalkopiadis <chalkopiadis.stratos@gmail.com>
2023-02-22 15:34:59 +0200

    idk

---
+++ Session-30/FuelStation/FuelStation.Blazor/Shared/ViewModels/UriViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.Blazor.Shared.ViewModels {
+    public class UriViewModel {
+        public string BaseAddress { get; set; }
+    }
+}
--- Session-30/FuelStation/FuelStation.Win/Program.cs
+++ Session-30/FuelStation/FuelStation.Win/Program.cs
@@ -1,8 +1,26 @@
+using Accessibility;
+using System.Net.NetworkInformation;
+using System;
+using System.ServiceProcess;
+using Microsoft.Extensions.DependencyInjection;
+using FuelStation.Blazor.Shared.Services;
+
 namespace FuelStation.Win {
     internal static class Program {
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+       public static IServiceProvider ServiceProvider { get; private set; }
+        private static void ConfigureServices() {
+            var services = new ServiceCollection();
+            //Actual Service
+            services.AddSingleton<LoginStatus>()
+                .AddScoped<AuthorizationHandler>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
+        }
         [STAThread]
         static void Main() {
             // To customize application configuration such as set high DPI settings or default font,
